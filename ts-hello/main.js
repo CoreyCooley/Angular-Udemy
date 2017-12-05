ce39e9baba8d086aa64750737438d4677139d15d
Corey Cooley <corey.a.cooley@gmail.com>
2017-12-04 22:54:34 -0500

    Add TypeScript Fundamentals

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -3,3 +3,23 @@ function log(message) {
 }
 var message = 'Hello World';
 log(message);
+DoSomething();
+function DoSomething() {
+    var number = 1;
+    var count = 2;
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+var doLog = function (message) {
+    console.log(message);
+};
+doLog("Corey Arrow Funtion");
+var drawPoint = function (point) {
+    console.log(point.x + ' ' + point.y);
+};
+drawPoint({
+    x: 1,
+    y: 2
+});
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -6,3 +6,38 @@ function log(message)
 var message = 'Hello World';
 
 log(message);
+DoSomething();
+
+
+function DoSomething()
+{
+    var number = 1;
+    let count  = 2;
+
+    for(let i = 0;  i < 5; i++)
+    {
+        console.log(i);
+    }
+
+    console.log('Finally: ' + i);
+}
+
+let doLog = (message) => {
+    console.log(message);
+}
+
+doLog("Corey Arrow Funtion");
+
+interface Point{
+    x: number,
+    y: number
+}
+
+let drawPoint = (point: Point)=>  {
+    console.log(point.x + ' ' + point.y);
+}
+
+drawPoint({
+    x:1,
+    y: 2
+})
